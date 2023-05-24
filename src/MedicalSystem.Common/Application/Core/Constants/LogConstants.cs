84200ac50d67866633ec1147f4727809a0359679
César Galvis <cgalvist@gmail.com>
2023-05-24 16:26:10 -0500

    feat: added serilog constants

---
+++ src/MedicalSystem.Common/Application/Core/Constants/LogConstants.cs
@@ -0,0 +1,35 @@
+namespace It270.MedicalSystem.Common.Application.Core.Constants;
+
+/// <summary>
+/// Constants for Logs Management
+/// </summary>
+public class LogConstants
+{
+    #region General
+
+    /// <summary>
+    /// Default project name
+    /// </summary>
+    public const string EmptyProjectName = "EmptyProjectName";
+
+    /// <summary>
+    /// Default storage database name
+    /// </summary>
+    public const string DefaultDbName = "logs";
+
+    #endregion
+
+    #region Custom attributes
+
+    /// <summary>
+    /// Application Name property value
+    /// </summary>
+    public const string ApplicationName = "ApplicationName";
+
+    /// <summary>
+    /// Custom record property value
+    /// </summary>
+    public const string CustomRecord = "CustomRecord";
+
+    #endregion
+}
