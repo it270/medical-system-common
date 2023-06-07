cacf85463b436e314ce1511ee9d8944e2a1c6dc8
César Galvis <cgalvist@gmail.com>
2023-06-07 16:29:24 -0500

    feat: added common IAM code

---
+++ src/MedicalSystem.Common/Application/Core/Helpers/User/IamRole.cs
@@ -0,0 +1,17 @@
+namespace It270.MedicalSystem.Common.Application.Core.Helpers.UserNS;
+
+/// <summary>
+/// IAM Role data (Cognito)
+/// </summary>
+public class IamRole
+{
+    /// <summary>
+    /// Role name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Role description
+    /// </summary>
+    public string Description { get; set; }
+}
