6913556116779d0b772b1780a427907499b67a94
César Galvis <cgalvist@gmail.com>
2023-06-09 08:43:59 -0500

    feat: added common user classes

---
+++ src/MedicalSystem.Common/Application/Core/Helpers/User/LogInData.cs
@@ -0,0 +1,22 @@
+namespace It270.MedicalSystem.Common.Application.Core.Helpers.UserNS;
+
+/// <summary>
+/// Log in basic data
+/// </summary>
+public class LogInData
+{
+    /// <summary>
+    /// User name
+    /// </summary>
+    public string UserName { get; set; }
+
+    /// <summary>
+    /// User password
+    /// </summary>
+    public string Password { get; set; }
+
+    /// <summary>
+    /// Client language
+    /// </summary>
+    public string Language { get; set; }
+}
---
+++ src/MedicalSystem.Common/Application/Core/Helpers/User/UserRoleRelationship.cs
@@ -0,0 +1,17 @@
+namespace It270.MedicalSystem.Common.Application.Core.Helpers.UserNS;
+
+/// <summary>
+/// User and Role relationship data
+/// </summary>
+public class UserRoleRelationship
+{
+    /// <summary>
+    /// User name
+    /// </summary>
+    public string UserName { get; set; }
+
+    /// <summary>
+    /// Role name
+    /// </summary>
+    public string Role { get; set; }
+}
