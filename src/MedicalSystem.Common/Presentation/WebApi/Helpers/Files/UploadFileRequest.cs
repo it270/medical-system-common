cbf0269f41c4e91679a6c643f84b2f78b390a50e
César Galvis <cgalvist@gmail.com>
2023-07-28 14:40:57 -0500

    feat: added file request attributes

--- src/MedicalSystem.Common/Presentation/WebApi/Helpers/Files/UploadFileRequest.cs
+++ src/MedicalSystem.Common/Presentation/WebApi/Helpers/Files/UploadFileRequest.cs
@@ -8,6 +8,21 @@ namespace It270.MedicalSystem.Common.Presentation.WebApi.Helpers.Files;
 public class UploadFileRequest
 {
     /// <summary>
+    /// General entity identifier
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// File name (optional)
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// File description (optional)
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
     /// General file
     /// </summary>
     public IFormFile File { get; set; }
