7ab3185f58b450a929343160519303ba2f98d8c9
César Galvis <cgalvist@gmail.com>
2023-08-30 14:09:05 -0500

    feat: improvements in swagger setup

--- src/MedicalSystem.Common/Presentation/WebApi/Config/ConfigOpenApi.cs
+++ src/MedicalSystem.Common/Presentation/WebApi/Config/ConfigOpenApi.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Swashbuckle.AspNetCore.Filters;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace It270.MedicalSystem.Common.Presentation.WebApi.Config;
@@ -15,10 +20,65 @@ namespace It270.MedicalSystem.Common.Presentation.WebApi.Config;
 public static class ConfigOpenApi
 {
     /// <summary>
+    /// Custom Swagger generator setup
+    /// </summary>
+    /// <param name="services">Service descriptors collection</param>
+    /// <param name="enableSecurity">Enable security setup</param>
+    /// <returns>Service descriptors collection with custom configuration</returns>
+    public static IServiceCollection AddSwaggerGenCustom(this IServiceCollection services,
+        bool enableSecurity = true)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            // Custom security configuration
+            if (enableSecurity)
+                options.ConfigDefaultSecurity();
+
+            // Enable Swagger filters
+            options.ExampleFilters();
+
+            try
+            {
+                // Add XML comments
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            }
+            catch (Exception)
+            {
+                Log.Error("XML comments for Swagger disabled");
+            }
+
+        });
+
+        // Enable automatic examples search in the project
+        services.AddSwaggerExamples();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Custom Swagger setup
+    /// </summary>
+    /// <param name="app">WebApplication instance</param>
+    /// <returns>WebApplication instance with custom configuration</returns>
+    public static WebApplication UseSwaggerCustom(this WebApplication app)
+    {
+        app.UseSwagger(c =>
+        {
+            c.PreSerializeFilters.Add((swagger, httpReq) =>
+            {
+                AddHosts(swagger, httpReq, app);
+            });
+        });
+
+        return app;
+    }
+
+    /// <summary>
     /// Default OpenAPI security
     /// </summary>
     /// <param name="options">Swagger options</param>
-    public static void ConfigDefaultSecurity(this SwaggerGenOptions options)
+    private static void ConfigDefaultSecurity(this SwaggerGenOptions options)
     {
         options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
@@ -50,10 +110,10 @@ public static class ConfigOpenApi
     /// </summary>
     /// <param name="swagger">Swagger document data</param>
     /// <param name="request">HTTP request</param>
-    /// <param name="builder">Web application builder</param>
-    public static void AddHosts(OpenApiDocument swagger, HttpRequest request, WebApplicationBuilder builder)
+    /// <param name="app">Web application</param>
+    private static void AddHosts(OpenApiDocument swagger, HttpRequest request, WebApplication app)
     {
-        var configValue = builder.Configuration
+        var configValue = app.Configuration
             .GetSection("Project:Servers")
             .Get<IList<OpenApiServer>>();
 
