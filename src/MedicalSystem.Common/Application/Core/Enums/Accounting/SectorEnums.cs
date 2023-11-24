11e8d992d51bb98cbebf744667a77353fad0d19c
César Galvis <cgalvist@gmail.com>
2023-11-23 19:19:59 -0500

    feat: added accounting sector enum

---
+++ src/MedicalSystem.Common/Application/Core/Enums/Accounting/SectorEnums.cs
@@ -0,0 +1,28 @@
+namespace It270.MedicalSystem.Common.Application.Core.Enums.Accounting;
+
+/// <summary>
+/// Accounting sector enums
+/// </summary>
+public static class SectorEnums
+{
+    /// <summary>
+    /// Accounting sector enum
+    /// </summary>
+    public enum SectorEnum : int
+    {
+        /// <summary>
+        /// Mixed sector
+        /// </summary>
+        Mixed = 1,
+
+        /// <summary>
+        /// Private sector
+        /// </summary>
+        Private,
+
+        /// <summary>
+        /// Public sector
+        /// </summary>
+        Public,
+    }
+}
