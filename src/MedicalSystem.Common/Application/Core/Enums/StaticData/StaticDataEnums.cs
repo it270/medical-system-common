792bd382ed4af8580f358baf40a98799dceb2539
César Galvis <cgalvist@gmail.com>
2023-11-30 10:31:04 -0500

    feat: added static data enums class

---
+++ src/MedicalSystem.Common/Application/Core/Enums/StaticData/StaticDataEnums.cs
@@ -0,0 +1,113 @@
+namespace It270.MedicalSystem.Common.Application.Core.Enums.StaticData;
+
+/// <summary>
+/// General static data enums
+/// </summary>
+public class StaticDataEnums
+{
+    /// <summary>
+    /// Enum group static
+    /// </summary>
+    public enum StaticGroupEnum : int
+    {
+        /// <summary>
+        /// Religion group
+        /// </summary>
+        Religion = 1,
+
+        /// <summary>
+        /// RH factor group
+        /// </summary>
+        RH,
+
+        /// <summary>
+        /// Language group
+        /// </summary>
+        Language,
+
+        /// <summary>
+        /// Marital status group
+        /// </summary>
+        MaritalStatus,
+
+        /// <summary>
+        /// Educational level group
+        /// </summary>
+        EducationalLevel,
+
+        /// <summary>
+        /// Special population group
+        /// </summary>
+        SpecialPopulation,
+
+        /// <summary>
+        /// Ethnic group
+        /// </summary>
+        EthnicGroup,
+
+        /// <summary>
+        /// Gender group
+        /// </summary>
+        Gender,
+
+        /// <summary>
+        /// Sex at birth group
+        /// </summary>
+        SexAtBirth,
+
+        /// <summary>
+        /// Job occupation group
+        /// </summary>
+        Occupation,
+
+        /// <summary>
+        /// Qualifying group
+        /// </summary>
+        Qualifying,
+
+        /// <summary>
+        /// Document type group
+        /// </summary>
+        DocumentType,
+
+        /// <summary>
+        /// Language level group
+        /// </summary>
+        LanguageLevel,
+
+        /// <summary>
+        /// Study status group
+        /// </summary>
+        StudyStatus,
+
+        /// <summary>
+        /// Study type CEI group
+        /// </summary>
+        StudyTypeCEI,
+
+        /// <summary>
+        /// Study phase CEI group
+        /// </summary>
+        StudyPhaseCEI,
+
+        /// <summary>
+        /// Company type group
+        /// </summary>
+        CompanyType,
+
+        /// <summary>
+        /// Study stage CEI group
+        /// </summary>
+        StudyStageCEI,
+
+        /// <summary>
+        /// Nationality group
+        /// </summary>
+        Nationality,
+
+        /// <summary>
+        /// Currency group
+        /// </summary>
+        Currency,
+    }
+}
