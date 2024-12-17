8b943cfd02697ee6f60ba47027ea0a3b66815867
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2024-12-17 14:58:14 -0500

    feat : update common static data

--- src/MedicalSystem.Common/Application/Core/Enums/StaticData/StaticDataEnums.cs
+++ src/MedicalSystem.Common/Application/Core/Enums/StaticData/StaticDataEnums.cs
@@ -1,248 +1,304 @@
-namespace It270.MedicalSystem.Common.Application.Core.Enums.StaticData;
-
-/// <summary>
-/// General static data enums
-/// </summary>
-public class StaticDataEnums
+namespace It270.MedicalSystem.Common.Application.Core.Enums.StaticData
 {
     /// <summary>
-    /// Enum group static
+    /// General static data enums
     /// </summary>
-    public enum StaticGroupEnum : int
+    public class StaticDataEnums
     {
         /// <summary>
-        /// Religion group
+        /// Enum group static
         /// </summary>
-        Religion = 1,
+        public enum StaticGroupEnum : int
+        {
+            /// <summary>
+            /// Religion group
+            /// </summary>
+            Religion = 1,
 
-        /// <summary>
-        /// RH factor group
-        /// </summary>
-        RH,
+            /// <summary>
+            /// RH group
+            /// </summary>
+            RH = 2,
 
-        /// <summary>
-        /// Language group
-        /// </summary>
-        Language,
+            /// <summary>
+            /// Language group
+            /// </summary>
+            Language = 3,
 
-        /// <summary>
-        /// Marital status group
-        /// </summary>
-        MaritalStatus,
+            /// <summary>
+            /// MaritalStatus group
+            /// </summary>
+            MaritalStatus = 4,
 
-        /// <summary>
-        /// Educational level group
-        /// </summary>
-        EducationalLevel,
+            /// <summary>
+            /// EducationalLevel group
+            /// </summary>
+            EducationalLevel = 5,
 
-        /// <summary>
-        /// Special population group
-        /// </summary>
-        SpecialPopulation,
+            /// <summary>
+            /// SpecialPopulation group
+            /// </summary>
+            SpecialPopulation = 6,
 
-        /// <summary>
-        /// Ethnic group
-        /// </summary>
-        EthnicGroup,
+            /// <summary>
+            /// EthnicGroup group
+            /// </summary>
+            EthnicGroup = 7,
 
-        /// <summary>
-        /// Gender group
-        /// </summary>
-        Gender,
+            /// <summary>
+            /// Gender group
+            /// </summary>
+            Gender = 8,
 
-        /// <summary>
-        /// Sex at birth group
-        /// </summary>
-        SexAtBirth,
+            /// <summary>
+            /// SexAtBirth group
+            /// </summary>
+            SexAtBirth = 9,
 
-        /// <summary>
-        /// Job occupation group
-        /// </summary>
-        Occupation,
+            /// <summary>
+            /// Occupation group
+            /// </summary>
+            Occupation = 10,
 
-        /// <summary>
-        /// Qualifying group
-        /// </summary>
-        Qualifying,
+            /// <summary>
+            /// Qualifying group
+            /// </summary>
+            Qualifying = 11,
 
-        /// <summary>
-        /// Document type group
-        /// </summary>
-        DocumentType,
+            /// <summary>
+            /// DocumentType group
+            /// </summary>
+            DocumentType = 12,
 
-        /// <summary>
-        /// Language level group
-        /// </summary>
-        LanguageLevel,
+            /// <summary>
+            /// LanguageLevel group
+            /// </summary>
+            LanguageLevel = 13,
 
-        /// <summary>
-        /// Study status group
-        /// </summary>
-        StudyStatus,
+            /// <summary>
+            /// StudyStatus group
+            /// </summary>
+            StudyStatus = 14,
 
-        /// <summary>
-        /// Study type CEI group
-        /// </summary>
-        StudyTypeCEI,
+            /// <summary>
+            /// StudyTypeCEI group
+            /// </summary>
+            StudyTypeCEI = 15,
 
-        /// <summary>
-        /// Study phase CEI group
-        /// </summary>
-        StudyPhaseCEI,
+            /// <summary>
+            /// StudyPhaseCEI group
+            /// </summary>
+            StudyPhaseCEI = 16,
 
-        /// <summary>
-        /// Company type group
-        /// </summary>
-        CompanyType,
+            /// <summary>
+            /// CompanyType group
+            /// </summary>
+            CompanyType = 17,
 
-        /// <summary>
-        /// Study stage CEI group
-        /// </summary>
-        StudyStageCEI,
+            /// <summary>
+            /// StudyStageCEI group
+            /// </summary>
+            StudyStageCEI = 18,
 
-        /// <summary>
-        /// Nationality group
-        /// </summary>
-        Nationality,
+            /// <summary>
+            /// Nationality group
+            /// </summary>
+            Nationality = 19,
 
-        /// <summary>
-        /// Currency group
-        /// </summary>
-        Currency,
+            /// <summary>
+            /// Currency group
+            /// </summary>
+            Currency = 20,
 
-        /// <summary>
-        /// AdmissionPathway group
-        /// </summary>
-        AdmissionPathway,
+            /// <summary>
+            /// AdmissionPathway group
+            /// </summary>
+            AdmissionPathway = 21,
 
-        /// <summary>
-        /// Priority group
-        /// </summary>
-        Priority,
+            /// <summary>
+            /// Priority group
+            /// </summary>
+            Priority = 22,
 
-        /// <summary>
-        /// AdmissionCause group
-        /// </summary>
-        AdmissionCause,
+            /// <summary>
+            /// AdmissionCause group
+            /// </summary>
+            AdmissionCause = 23,
 
-        /// <summary>
-        /// RelationshipType group
-        /// </summary>
-        RelationshipType,
+            /// <summary>
+            /// RelationshipType group
+            /// </summary>
+            RelationshipType = 24,
 
-        /// <summary>
-        /// Destination group
-        /// </summary>
-        Destination,
+            /// <summary>
+            /// Destination group
+            /// </summary>
+            Destination = 25,
 
-        /// <summary>
-        /// OptionYN group
-        /// </summary>
-        OptionYN,
+            /// <summary>
+            /// OptionYN group
+            /// </summary>
+            OptionYN = 26,
 
-        /// <summary>
-        /// Purpose group
-        /// </summary>
-        Purpose,
+            /// <summary>
+            /// Purpose group
+            /// </summary>
+            Purpose = 27,
 
-        /// <summary>
-        /// Diagnosis group
-        /// </summary>
-        Diagnosis,
+            /// <summary>
+            /// Diagnosis group
+            /// </summary>
+            Diagnosis = 28,
 
-        /// <summary>
-        /// DiagnosticType group
-        /// </summary>
-        DiagnosticType,
+            /// <summary>
+            /// DiagnosticType group
+            /// </summary>
+            DiagnosticType = 29,
 
-        /// <summary>
-        /// Classification group
-        /// </summary>
-        Classification,
+            /// <summary>
+            /// Classification group
+            /// </summary>
+            Classification = 30,
 
-        /// <summary>
-        /// ViaAdministration group
-        /// </summary>
-        ViaAdministration,
+            /// <summary>
+            /// ViaAdministration group
+            /// </summary>
+            ViaAdministration = 31,
 
-        /// <summary>
-        /// PharmaceuticalForm group
-        /// </summary>
-        PharmaceuticalForm,
+            /// <summary>
+            /// PharmaceuticalForm group
+            /// </summary>
+            PharmaceuticalForm = 32,
 
-        /// <summary>
-        /// UnitAdministration group
-        /// </summary>
-        UnitAdministration,
+            /// <summary>
+            /// UnitAdministration group
+            /// </summary>
+            UnitAdministration = 33,
 
-        /// <summary>
-        /// DispatchUnit group
-        /// </summary>
-        DispatchUnit,
+            /// <summary>
+            /// DispatchUnit group
+            /// </summary>
+            DispatchUnit = 34,
 
-        /// <summary>
-        /// Frequency group
-        /// </summary>
-        Frequency,
+            /// <summary>
+            /// Frequency group
+            /// </summary>
+            Frequency = 35,
 
-        /// <summary>
-        /// TypeMedicationRequest group
-        /// </summary>
-        TypeMedicationRequest,
+            /// <summary>
+            /// TypeMedicationRequest group
+            /// </summary>
+            TypeMedicationRequest = 36,
 
-        /// <summary>
-        /// During group
-        /// </summary>
-        During,
+            /// <summary>
+            /// During group
+            /// </summary>
+            During = 37,
 
-        /// <summary>
-        /// TypeDisability group
-        /// </summary>
-        TypeDisability,
+            /// <summary>
+            /// TypeDisability group
+            /// </summary>
+            TypeDisability = 38,
 
-        /// <summary>
-        /// LevelDisability group
-        /// </summary>
-        LevelDisability,
+            /// <summary>
+            /// LevelDisability group
+            /// </summary>
+            LevelDisability = 39,
 
-        /// <summary>
-        /// TypeDisabilityForm group
-        /// </summary>
-        TypeDisabilityForm,
+            /// <summary>
+            /// TypeDisabilityForm group
+            /// </summary>
+            TypeDisabilityForm = 40,
 
-        /// <summary>
-        /// PrimaryReason group
-        /// </summary>
-        PrimaryReason,
+            /// <summary>
+            /// PrimaryReason group
+            /// </summary>
+            PrimaryReason = 41,
 
-        /// <summary>
-        /// RetroactiveOption group
-        /// </summary>
-        RetroactiveOption,
+            /// <summary>
+            /// RetroactiveOption group
+            /// </summary>
+            RetroactiveOption = 42,
 
-        /// <summary>
-        /// ModalityDisability group
-        /// </summary>
-        ModalityDisability,
+            /// <summary>
+            /// ModalityDisability group
+            /// </summary>
+            ModalityDisability = 43,
 
-        /// <summary>
-        /// DisabilityOrigin group
-        /// </summary>
-        DisabilityOrigin,
+            /// <summary>
+            /// DisabilityOrigin group
+            /// </summary>
+            DisabilityOrigin = 44,
 
-        /// <summary>
-        /// SiteTemperature group
-        /// </summary>
-        SiteTemperature,
+            /// <summary>
+            /// SiteTemperature group
+            /// </summary>
+            SiteTemperature = 45,
 
-        /// <summary>
-        /// Egress group
-        /// </summary>
-        Egress,
+            /// <summary>
+            /// Egress group
+            /// </summary>
+            Egress = 46,
 
-        /// <summary>
-        /// Egress group
-        /// </summary>
-        ProcedureType,
+            /// <summary>
+            /// ProcedureType group
+            /// </summary>
+            ProcedureType = 47,
+
+            /// <summary>
+            /// TestOciones group
+            /// </summary>
+            TestOciones = 48,
+
+            /// <summary>
+            /// Zone group
+            /// </summary>
+            Zone = 49,
+
+            /// <summary>
+            /// Intervention group
+            /// </summary>
+            Intervention = 50,
+
+            /// <summary>
+            /// Interconsultation group
+            /// </summary>
+            Interconsultation = 51,
+
+            /// <summary>
+            /// Listen group
+            /// </summary>
+            Listen = 52,
+
+            /// <summary>
+            /// Needs group
+            /// </summary>
+            Needs = 53,
+
+            /// <summary>
+            /// When group
+            /// </summary>
+            When = 54,
+
+            /// <summary>
+            /// ContactNumber group
+            /// </summary>
+            ContactNumber = 55,
+
+            /// <summary>
+            /// Label group
+            /// </summary>
+            Label = 56,
+
+            /// <summary>
+            /// ConsultationType group
+            /// </summary>
+            ConsultationType = 57,
+
+            /// <summary>
+            /// ControlNumber group
+            /// </summary>
+            ControlNumber = 58
+        }
     }
 }
