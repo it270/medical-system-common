76e88620ebf42d05ff5955b8d9525cb159bbd716
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2025-05-21 21:01:01 -0500

    feat : EmailMessageWithAttachment

---
+++ src/MedicalSystem.Common/Application/Core/Contracts/EmailMessageWithAttachment.cs
@@ -0,0 +1,14 @@
+﻿namespace It270.MedicalSystem.Common.Application.Core.Contracts
+{
+    // Usar 'record' para inmutabilidad y concisión en mensajes
+    public record EmailMessageWithAttachment // Ya no es 'I', porque es una implementación de datos
+    (
+        string Email,
+        string Name,
+        string Subject,
+        string Content, // HTML content
+        string? AttachmentUrl, // URL del adjunto en el storage
+        string? AttachmentFileName, // Nombre original del adjunto
+        string? AttachmentContentType // Tipo de contenido original del adjunto
+    );
+}
