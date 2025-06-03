0cb3472305057627a4aeb1435456ad691a3a555a
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2025-06-03 15:02:46 -0500

    feat : ethics pdf

---
+++ src/MedicalSystem.Common/Application/Core/Entities/PDFinfo/PDFinfo.cs
@@ -0,0 +1,54 @@
+﻿using It270.MedicalSystem.Common.Application.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace It270.MedicalSystem.Common.Application.Core.Entities.PDFinfo
+{
+    public class PDFinfo
+    {
+        public int CenterId { get; set; }
+        public string CenterName { get; set; }
+        public string CenterLogo { get; set; }
+        public string CenterNit { get; set; }
+        public string CenterCity { get; set; }
+        public string CenterPresidentName { get; set; }
+        public string Protocol { get; set; }
+        public List<StudySubmissionObservation> Description { get; set; }
+        public string StudyPhase { get; set; }
+        public List<Subject> Subjects { get; set; }
+        public string SigningInvestigatorName { get; set; }
+        public string FilingNumber { get; set; }
+        public string CenterAddress { get; set; }
+    }
+
+    public class Subject
+    {
+        public int Id { get; set; }
+        public int CiomsId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string Observacion { get; set; }
+        public string TrackingNumber { get; set; }
+        public string SubjectNumber { get; set; }
+        public string Event { get; set; }
+        public int CountryId { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public int CiomsReportCausalityId { get; set; }
+        public int CiomsReportTypeId { get; set; }
+    }
+
+    public class StudySubmissionObservation
+    {
+        public int Id { get; set; }
+        public int StudySubmissionId { get; set; }
+        public string Note { get; set; }
+        public int CreatedByUserId { get; set; }
+        public int UpdatedByUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
