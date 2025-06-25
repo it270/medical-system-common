3a45171c0e909a7599fd8a8aae33ef7bbd346281
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2025-06-24 21:35:25 -0500

    feat : nullable pdf

--- src/MedicalSystem.Common/Application/Core/Entities/PDFinfo/PDFinfo.cs
+++ src/MedicalSystem.Common/Application/Core/Entities/PDFinfo/PDFinfo.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 
 public class PDFInfoCommon
 {
-    public int CenterId { get; set; }
-    public string CenterName { get; set; }
-    public string CenterLogo { get; set; }
-    public string CenterNit { get; set; }
-    public string CenterCity { get; set; }
-    public string CenterPresidentName { get; set; }
-    public string Protocol { get; set; }
-    public string Description { get; set; }
-    public string StudyPhase { get; set; } // null puede ser string o un tipo personalizado si lo defines después
-    public List<Subject> Subjects { get; set; }
-    public string SigningInvestigatorName { get; set; }
-    public string FilingNumber { get; set; }
-    public string CenterAddress { get; set; }
+    public int? CenterId { get; set; }
+    public string? CenterName { get; set; }
+    public string? CenterLogo { get; set; }
+    public string? CenterNit { get; set; }
+    public string? CenterCity { get; set; }
+    public string? CenterPresidentName { get; set; }
+    public string? Protocol { get; set; }
+    public string? Description { get; set; }
+    public string? StudyPhase { get; set; } // null puede ser string o un tipo personalizado si lo defines después
+    public List<Subject>? Subjects { get; set; }
+    public string? SigningInvestigatorName { get; set; }
+    public string? FilingNumber { get; set; }
+    public string? CenterAddress { get; set; }
 }
 
 public class Subject
 {
-    public int Id { get; set; }
-    public int CiomsId { get; set; }
-    public string FileName { get; set; }
-    public string FilePath { get; set; }
-    public string Observacion { get; set; }
-    public string TrackingNumber { get; set; }
-    public string SubjectNumber { get; set; }
-    public string Event { get; set; }
-    public long CountryId { get; set; }
-    public DateTime EventDate { get; set; }
-    public DateTime ReceiptDate { get; set; }
-    public int CiomsReportCausalityId { get; set; }
-    public int CiomsReportTypeId { get; set; }
+    public int? Id { get; set; }
+    public int? CiomsId { get; set; }
+    public string? FileName { get; set; }
+    public string? FilePath { get; set; }
+    public string? Observacion { get; set; }
+    public string? TrackingNumber { get; set; }
+    public string? SubjectNumber { get; set; }
+    public string? Event { get; set; }
+    public long? CountryId { get; set; }
+    public DateTime? EventDate { get; set; }
+    public DateTime? ReceiptDate { get; set; }
+    public int? CiomsReportCausalityId { get; set; }
+    public int? CiomsReportTypeId { get; set; }
 }
